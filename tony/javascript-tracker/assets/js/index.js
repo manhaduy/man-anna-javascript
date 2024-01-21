18bacf78afbec431fe99389c89e597c4b252f4c9
Tony Nguyen <tony.nguyen@usonialabs.com>
2024-01-21 21:33:24 +0700

    add UI

---
+++ tony/javascript-tracker/assets/js/index.js
@@ -0,0 +1,6 @@
+const descriptionInput = document.getElementById('description');
+const buttonAdd = document.getElementById('buttonAdd');
+
+buttonAdd.addEventListener('click', function handleAdd() {
+  console.log(descriptionInput.value)
+});
